c8aee79afa89e48f605b0f9872467f97b7755947
Alexandre Lima <48870020+alexandrezlima@users.noreply.github.com>
2024-05-17 12:23:47 -0300

    Update playground.js

--- playground.js
+++ playground.js
@@ -1,281 +1,285 @@
-<script>
-let Application;
-import * as PixelStreamingWebSdk from "https://unpkg.com/@arcware-cloud/pixelstreaming-websdk@latest/index.esm.js";
-(() => {
-  const { ArcwareInit } = PixelStreamingWebSdk;
-  const initResult = new ArcwareInit(
-    {
-      shareId: "share-5db6829b-ec92-4651-b90b-b725c75bd5f1"
-    },
-    {
-      initialSettings: {
-        StartVideoMuted: true,
-        AutoConnect: true,
-        AutoPlayVideo: true,
-        SuppressBrowserKeys: false
-      },
-      settings: {
-        fullscreenButton: false,
-        infoButton: false,
-        micButton: false,
-        audioButton: true,
-        settingsButton: false,
-        connectionStrengthIcon: false,
-        session: ""
-      },
-    }
-  );
 
-  Application = initResult.Application;
+        import * as PixelStreamingWebSdk from "https://unpkg.com/@arcware-cloud/pixelstreaming-websdk@latest/index.esm.js";
 
-  let PixelStreaming = initResult.PixelStreaming;
+        (() => {
+        const { ArcwareInit } = PixelStreamingWebSdk;
+        const initResult  = ArcwareInit(
+            {
+            shareId: "share-5db6829b-ec92-4651-b90b-b725c75bd5f1"
+            },
+            {
+                initialSettings: {
+                    StartVideoMuted: false,
+                    AutoConnect: true,
+                    AutoPlayVideo: true,
+                    SuppressBrowserKeys: true,
+                    AFKTimeout: 360,
+                    TimeoutIfIdle: 360
+                },
+                settings: {
+                    infoButton: false,
+                    micButton: false,
+                    audioButton: false,
+                    fullscreenButton: false,
+                    settingsButton: false,
+                    connectionStrengthIcon: false,
+                    SuppressBrowserKeys: false,
+                    session: ""
+                },
+            }
+        );
 
+        let Application = initResult.Application;
+        let PixelStreaming = initResult.PixelStreaming;
 
+        if (localStorage.getItem('url'))
+        {
+          const newUrl = localStorage.getItem('url');
+          history.replaceState(null, null, newUrl);
+          localStorage.removeItem('url');
+        }
+        
+        
+        let counter = 0;
 
+        
 
-  PixelStreaming.videoInitializedHandler.add(
-    () => {
-      console.log("Video initialized.");
-
-      setTimeout(() => {
-        removeLoadingScreen();
-
-        // Função para criar o popup personalizado com bordas arredondadas
-        function createPermissionPopup() {
-            // Cria o elemento do popup
-            var popup = document.createElement('div');
-            
-            // Define estilos do popup
-            popup.style.position = 'fixed';
-            popup.style.top = '50%';
-            popup.style.left = '50%';
-            popup.style.transform = 'translate(-50%, -50%)';
-            popup.style.padding = '20px';
-            popup.style.backgroundColor = '#f9f9f9';
-            popup.style.borderRadius = '15px';
-            popup.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
-            popup.style.zIndex = '9999';
-            
-            // Mensagem do popup
-            var message = document.createElement('p');
-            message.innerText = 'Deseja permitir a ativação do som?';
-            popup.appendChild(message);
-            
-            // Botão para negar
-            var denyButton = document.createElement('button');
-            denyButton.innerText = "Don't Allow";
-            denyButton.style.marginRight = '10px';
-            denyButton.onclick = function () {
-                // Adicione lógica aqui para negar a ativação do som, se necessário
-                console.log('Permissão negada para ativar o som');
-                document.body.removeChild(popup);
-            };
-            popup.appendChild(denyButton);
-            
-            // Botão para permitir
-            var allowButton = document.createElement('button');
-            allowButton.innerText = 'OK';
-            allowButton.onclick = function () {
-                // Adicione lógica aqui para permitir a ativação do som, por exemplo, ativar elementos de áudio ou vídeo
-                var videoElement = document.getElementById('videoRef');
-                var audioElement = document.getElementById('audioRef');
-                if (videoElement) {
-                    videoElement.muted = false;
-                    videoElement.play();
-                }
-                if (audioElement) {
-                    audioElement.muted = false;
-                    audioElement.play();
-                }
-                console.log('Permissão concedida para ativar o som');
-                document.body.removeChild(popup);
-            };
-            popup.appendChild(allowButton);
-            
-            // Adiciona o popup ao corpo do documento
-            document.body.appendChild(popup);
+        //Internal functions.
+        window.handleSendCommands = function(command) {
+            if (Application)
+                Application.emitUIInteraction(command);
         }
 
-        // Função para verificar se o elemento de áudio ou vídeo está silenciado
-        function checkMuteStatus() {
-            var videoElement = document.getElementById('videoRef');
-            var audioElement = document.getElementById('audioRef');
+        //Removes the loading screen.
+        window.removeLoadingScreen = function() {
+            const overlay = document.getElementById('overlay');
+            const imageContainer = document.getElementById('imageContainer');
+            
+            if (overlay)
+                overlay.remove();
 
-            if (videoElement && audioElement) {
-                // Verifica se o vídeo ou áudio está silenciado
-                if (videoElement.muted || audioElement.muted) {
-                    // Se estiverem silenciados, exibe o popup
-                    createPermissionPopup();
-                }
+            if (imageContainer)
+                imageContainer.remove();
+        }
+
+        //Handles the url to open in a new tab.
+        window.handleOpenLink = function(url) {
+            window.open(url, "_blank");
+            console.log(url);
+        }
+
+        //Handles the paste function event.
+        document.addEventListener("paste", function(event) {
+            //Get the clipboard contents as plain text
+            var clipboardData = event.clipboardData || window.clipboardData;
+            var pastedText = clipboardData.getData("text/plain");
+
+            //Sends the pasted text to the pixel streaming application.
+            const command = { paste: pastedText };
+            handleSendCommands(command);
+        });
+
+        //Used to change the cursor type on interactive elements such as buttons.
+        window.toggleCursor = function(cursortype) {
+            const videoElement = document.querySelector('video');
+            if (videoElement) {
+                videoElement.style.cursor = cursortype;
+            }
+        }
+        
+        //Returns if the streaming video is muted.
+        window.audiostatus = function()
+        {
+            //Gets the first video element. The streaming.
+            const videoElement = document.querySelector('video');
+
+            if (videoElement) {
+                const isMuted = videoElement.muted;
+                console.log(isMuted ? "muted" : "not muted");
+
+                const command = { audiostatus: isMuted ? "muted" : "not muted" };
+                handleSendCommands(command);
+            } else {
+                console.log("Nenhum elemento de vídeo encontrado na página.");
             }
         }
 
-    checkMuteStatus();
+        window.unmutesound = function()
+        {
+            //Gets the first video element. The streaming.
+            const videoElement = document.querySelector('video');
+            
+            if (videoElement) {
+                //Checks if the video is muted.
+                videoElement.muted = false;
+                videoElement.autoplay = true;
 
+                videoElement.play();
+                document.cookie = "autoplay=true; path=/; max-age=31536000";
+            } else {
+                console.log("No video elements found.");
+            }
+        }
 
+        //Returns if there is any segment.
+        window.getSegment = function() {
+            let name = 'segment';
 
-      }, 1500);
-    });
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const paramValue = urlParams.get(name);
 
+            let segmentValue = (paramValue !== null) ? paramValue : -1;
 
-  Application.getApplicationResponse(
-    (response) => {
+            const command = { segment: segmentValue };
+            handleSendCommands(command);            
+        }
 
-      /*********** CUSTOM CODE BEGINS *****************************************************/
-      const url1 = JSON.parse(response); //Gets the response and parses JSON.
+        //Autorecords the screen after loading the level.
+        window.autoRecord = function() {
+            let name = 'ar';
 
-      const levelname = "L_CiscoLive24";
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const paramValue = urlParams.get(name);
 
-      //Removing loading response.
-      if (url1.url == "removeloading")
-      {
-        removeLoadingScreen();
-        const command = { loadingremoved: 'loadingremoved' };
-        handleSendCommand(command);
-      }
+            if (paramValue === 'true')
+            {
+                const command = { autorecord: paramValue };
+                handleSendCommands(command);
+            }
+            else
+            {
+								const command = { autorecord: "false" };
+                handleSendCommands(command);
+            }
+        }
 
-      //Get level response.
-      else if (url1.url == "getlevel")
-      {
-        /*
-        //Gets the tab URL, looking for a level name.
-        const currentPageUrl = window.location.href;
-        let level = "";
-        const lastSlashIndex = currentPageUrl.lastIndexOf('/');
-        const questionMarkIndex = currentPageUrl.lastIndexOf('?');
-        if (questionMarkIndex !== -1)
-          level = currentPageUrl.substring(lastSlashIndex + 1, questionMarkIndex);
-        else
-          level = currentPageUrl.substring(lastSlashIndex + 1);
-      
-        //const command = { loadlevel: level }; //Use this line instead of below to open the link level
-        */
-        const command = { loadlevel: levelname };
-        handleSendCommand(command);
-      }
+        window.viewMode = function() {
+            let name = 'vo';
 
-      //Get filename response. Responsible to get the dropbox filename in the query. Marked to edit.
-      else if (url1.url == "getfilename")
-      {
-        //Finds the value of ?<value>.
-        const currentPageUrl = window.location.href;
-        const questionMarkIndex = currentPageUrl.lastIndexOf('?');
-        const equalSignIndex = currentPageUrl.lastIndexOf('=');
-        let filename = '';
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const paramValue = urlParams.get(name);
 
-        if (questionMarkIndex !== -1 && equalSignIndex !== -1)
-          filename = currentPageUrl.substring(questionMarkIndex + 1, equalSignIndex);
-        else
-          filename = '';
-
-        const command = { loadfilename: filename };
-        handleSendCommand(command);
-      }
-
-      //Get filename response. Responsible to get the dropbox key in the query. Marked to edit.
-      else if (url1.url == "getfilekey")
-      {
-        const currentPageUrl = window.location.href;
-        let filekey = '';
-        const equalSignIndex = currentPageUrl.lastIndexOf('=');
-
-        if (equalSignIndex !== -1)
-          filekey = currentPageUrl.substring(equalSignIndex + 1);
-        else
-          filekey = '';
+            if (paramValue === "true")
+            {
+                const command = { viewonly: paramValue };
+                handleSendCommands(command);
+            }
+            else
+            {
+            		const command = { viewonly: "false" };
+                handleSendCommands(command);
+						}
+        }
         
-        const command = { loadfilekey: filekey };
-        handleSendCommand(command);
-      }
-
-      //Check level response.
-      else if(url1.url == "checklevel")
-      {
-        const command = { checklevel: levelname };
-        handleSendCommand(command);
-      }
-
-      //Sets the cursor type to "default".
-      else if(url1.url == "setcursordefault")
-      {
-        toggleCursor("default");
-      }
-
-      //Sets the cursor type to "hand".
-      else if(url1.url == "setcursorhand")
-      {
-        toggleCursor("pointer");
-      }
-
-      //Most important event: opens urls. 
-      else
-      {
-        handleOpenLink(url1.url);
-      }
-    /*********** CUSTOM CODE ENDS ******************************************************/
-
-  });
-
-  setTimeout(() =>
-    document
-      .getElementById("videoContainer")
-      .appendChild(Application.rootElement)
-  );
-
-})();
-
-window.handleSendCommand = function(command) {
-  if (Application) {
-    Application.emitUIInteraction(command);
-  }
-}
+        window.updateProgressBarPercentage = function(percent) {
+            const barProgress = document.getElementById('loadingProgressBar');
+            if (barProgress) {
+            		if (counter === 0) {
+                    const loadingBar = document.getElementById('loadingBar');
+                    if (loadingBar) {
+                        loadingBar.style.display = 'block';
+                    }
+                    counter++;
+                }
+            
+                barProgress.style.width = percent + '%';
+            }
+        }
 
 
-/*********** CUSTOM CODE ***********************************************************/
-//Toggles the cursor style.
-function toggleCursor(cursortype) {
-  const videoContainer = document.getElementById("videoContainer");
-  console.log("videoContainer:", videoContainer);
-  console.log("cursortype:", cursortype);
+        //Called when the video streaming starts.
+        PixelStreaming.videoInitializedHandler.add(
+        () => {
+          console.log("Video initialized.");
 
-  if (cursortype === "pointer") {
-      videoContainer.style.cursor = "pointer";
-  } else if (cursortype === "default") {
-      videoContainer.style.cursor = "default";
-  }
+            setTimeout(() => {
+                audiostatus();
+            }, 4000);
+        });
+        
+        PixelStreaming.websocketOnCloseHandler.add(() => {
+            console.log("Disconnected.");
+            const url = window.location.href;
+						localStorage.setItem('url', url);
+            
+            window.location.href = 'https://www.mile80.com/eventplayground/reload';
+        });
+        
 
-  console.log("Cursor style set to:", videoContainer.style.cursor);
-}
+        //Responses from UE application.
+        Application.getApplicationResponse(
+            (response) => {
+            console.log("ApplicationResponse", response);
 
+            const url1 = JSON.parse(response);
 
-//Handles the paste function event
-document.addEventListener("paste", function(event) {
-  //Get the clipboard contents as plain text
-  var clipboardData = event.clipboardData || window.clipboardData;
-  var pastedText = clipboardData.getData("text/plain");
+            /*SET HERE THE LEVEL NAME*/
+            const levelname = "L_Upfronts24_0408";
 
-  //Sends the pasted text to the pixel streaming application.
-  const command = { paste: pastedText };
-  handleSendCommand(command);
-});
+            //Removing loading response.
+            if (url1.url == "removeloading")
+            {
+                removeLoadingScreen();
+                const command = { loadingremoved: 'loadingremoved' };
+                handleSendCommands(command);
+            }
 
+            //Returns the level to load.
+            else if (url1.url == "getlevel")
+            {
+                const command = { loadlevel: levelname };
+                handleSendCommands(command);
+            }
 
-//Opens the URL link on the browser (new tab)
-function handleOpenLink(url)
-{
-  window.open(url, "_blank");
-  console.log(url);
-}
+            //Returns the current level name.
+            else if(url1.url == "checklevel")
+			{
+                const command = { checklevel: levelname };
+                handleSendCommands(command);
+			}
 
-//If called, removes the loading screen overlay and the loading image
-function removeLoadingScreen() {
-  const image = document.querySelector('.centered-image');
-  if (image) {
-      image.remove();
-      overlay.remove();
-  }
-}
+            //Sets the cursor type to default.
+            else if(url1.url == "setcursordefault")
+            {
+                toggleCursor("default");
+            }
 
+            //Sets the cursor type to hand.
+            else if(url1.url == "setcursorhand")
+            {
+                toggleCursor("pointer");
+            }
 
-/*********** CUSTOM CODE ENDS ******************************************************/
-</script>
+            //Allows volume to true.
+            else if (url1.url == "unmutesound")
+            {
+                unmutesound();
+                document.cookie = "audioPermission=true; path=/; max-age=31536000";
+            }
+
+            else if (url1.url == "queries")
+            {
+                viewMode();
+                autoRecord();
+                getSegment();
+                console.log("Queries done.");
+            }
+            
+            else if (url1.progress)
+            {
+                updateProgressBarPercentage(url1.progress);
+                console.log(url1.progress);
+            }
+
+            //Open the link in a new tab.
+            else
+            {
+                handleOpenLink(url1.url);
+            }
+        });
+
+        document.getElementById("video-container").appendChild(Application.rootElement);
+    })();
